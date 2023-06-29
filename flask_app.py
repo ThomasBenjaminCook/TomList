12fbc8932e5c7025f2ef0e03fe41fe02f608547f
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-30 02:40:57 +1000

    maybe this will overwrite wtf

--- flask_app.py
+++ flask_app.py
@@ -36,7 +36,7 @@ class Kart(FlaskForm):
     submit1 = SubmitField('Submit')
 
 class Remove(FlaskForm):
-    submit2 = SubmitField()
+    submit2 = SubmitField("Done", name="usern")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'u3ygfr7evyguyg87y6fuev$%^&^%$'
