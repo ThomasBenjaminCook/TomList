8e368fd7c6fd09ae626509c578c2bbecbbbf5452
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-29 10:55:18 +1000

    insert based on python list

--- flask_app.py
+++ flask_app.py
@@ -14,6 +14,42 @@ with open(THIS_FOLDER / "page1.txt") as f:
     page1 = f.readlines()
 page1 = (" ").join(page1)
 
+def stringinserter(tag,string,insertables):
+    array = string.split(tag)
+    outputarray = []
+    for x in range(len(array)):
+        outputarray.append(array[x])
+        if x < len(insertables):
+            outputarray.append(insertables[x])
+    return(("").join(outputarray))
+
 @app.route("/")
 def data():
-    return page1
+
+    all_items = ["milk","bread","cheese","chocolate"]
+    shops = {"milk":"ALDI","bread":"ALDI","cheese":"ALDI","chia":"Coles","chocolate":"Coles"}
+    shopping_list = ["milk","bread","chia"]
+
+    first_layer = []
+
+    coles_string = " "
+    ALDI_string = " "
+
+    for item in shopping_list:
+        if(shops[item] == "ALDI"):
+            ALDI_string = ALDI_string + '<input type="submit" value='+item+'><br><br>'
+        else:
+            coles_string = coles_string + '<input type="submit" value='+item+'><br><br>'
+
+    first_layer.append(ALDI_string)
+    first_layer.append(coles_string)
+
+    option_string = " "
+
+    for item in all_items:
+        option_string = option_string + '<option value='+item+'>'
+
+    first_layer.append(option_string)
+    first_layer.append(option_string)
+
+    return (stringinserter("@",page1,first_layer))
