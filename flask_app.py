037f12b1c1c3de7ecdd16bea4d8fa21e1a4aebca
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-07-01 01:20:06 +1000

    try setting same index before concat

--- flask_app.py
+++ flask_app.py
@@ -89,8 +89,9 @@ def data():
         corresponding_shop = add_form.shopper.data
         next_index = get_next_index(total_list_dataframe)
         row = pd.DataFrame({"itemID": next_index, "item": newer_item, "shop" : corresponding_shop},index=[next_index])
-        total_list_dataframe = pd.concat([total_list_dataframe,row], axis=0)
+        row.set_index('itemID', inplace=True)
         total_list_dataframe.set_index('itemID', inplace=True)
+        total_list_dataframe = pd.concat([total_list_dataframe,row], axis=0)
         total_list_dataframe.to_sql("all_items", con=engine, if_exists="replace",index_label="itemID")
         all_items = total_list_dataframe["item"].to_list()
         shops = dataframe_to_dict(total_list_dataframe,"item","shop")
