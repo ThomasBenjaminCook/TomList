6e8073e052b629a2ef02338e884a9e5ea14620d2
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-30 14:00:07 +1000

    just want to see what specific remove is

--- flask_app.py
+++ flask_app.py
@@ -89,10 +89,11 @@ def data():
         target_index = shopping_list_indicies[count]
         if(shops[item] == "ALDI"):
             weird_id = "ALDI_"+str(target_index)
-            ALDI_string = ALDI_string + "{% set specific_remove = remove_form.__setattr__('strange', '"+weird_id+"') %} {{ specific_remove.submit2() }}</br></br>"
+            #ALDI_string = ALDI_string + "{% set specific_remove = remove_form.__setattr__('strange', '"+weird_id+"') %} {{ specific_remove.submit2() }}</br></br>"
+            ALDI_string = ALDI_string + "{% set specific_remove = remove_form.__setattr__('strange', '"+weird_id+"') %} {{ specific_remove }}</br></br>"
         if(shops[item] == "Coles"):
             weird_id = "Coles_"+str(target_index)
-            coles_string = coles_string + "{% set specific_remove = remove_form.__setattr__('strange', '"+weird_id+"') %} {{ specific_remove.submit2() }}</br></br>"
+            #coles_string = coles_string + "{% set specific_remove = remove_form.__setattr__('strange', '"+weird_id+"') %} {{ specific_remove.submit2() }}</br></br>"
         count = count + 1
 
     first_layer.append(ALDI_string)
