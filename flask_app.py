dc217503562bdb77db67143569161adae0556980
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-06-30 23:15:46 +1000

    brute

--- flask_app.py
+++ flask_app.py
@@ -84,9 +84,10 @@ def data():
         newer_item = add_form.itemire.data
         corresponding_shop = add_form.shopper.data
         next_index = get_next_index(total_list_dataframe)
-        print(total_list_dataframe)
-        print(pd.DataFrame({"item": [newer_item],"shop":[corresponding_shop]}))
-        total_list_dataframe.loc[next_index] = pd.DataFrame({"item": [newer_item],"shop":[corresponding_shop]})
+        row = total_list_dataframe.iloc[0]
+        row["item"] = [newer_item]
+        row["shop"] = [corresponding_shop]
+        total_list_dataframe.loc[next_index] = row
         total_list_dataframe.to_sql("all_items", con=engine, if_exists="replace")
         all_items = total_list_dataframe["item"].to_list()
         shops = dataframe_to_dict(total_list_dataframe,"item","shop")
