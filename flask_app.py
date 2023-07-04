377bb0653ec062b051a7daf8beee68580020b781
Tom <123046600+ThomasBenjaminCook@users.noreply.github.com>
2023-07-04 23:44:51 +1000

    the remove button should do something

--- flask_app.py
+++ flask_app.py
@@ -73,8 +73,6 @@ def data():
     shopping_list = shopping_list_dataframe["item"].to_list()
     shopping_list_indicies = list(shopping_list_dataframe.index.values)
 
-    first_layer = []
-
     kart_form = Kart()
     if (kart_form.validate_on_submit() and kart_form.submit1.data):
         if(kart_form.itemer.data in all_items):
@@ -87,7 +85,6 @@ def data():
             shopping_list_dataframe.to_sql("shopping_list", con=engine, if_exists="replace")
         else:
             kart_form.itemer.data = ""
-
     
     add_form = Adder()
     if (add_form.validate_on_submit() and add_form.submit2.data):
@@ -131,7 +128,18 @@ def data():
         weird_ids.append(weird_id)
         count = count + 1
 
+    personal_remove_ids = []
+    personal_edit_ids = []
+    recipe_string = " "
+    for instruction in recipes_dataframe["instructions"]:
+        personal_remove_id = ("_").join(instruction.split(" "))
+        personal_remove_ids.append(personal_remove_id)
+        personal_edit_id = ("v").join(instruction.split(" "))
+        personal_edit_ids.append(personal_edit_id)
+        recipe_string = recipe_string + '<div id="recipe">' + instruction + "</br></br><form method='POST'><input type='submit' value='remove' name='"+personal_remove_id+"'/></br></br><input type='submit' value='edit' name='"+personal_edit_id+"'/></form></br></br></div></br>"   
+
     if request.method == "POST":
+
         count = 0
         while count < len(shopping_list):
             weird_id = weird_ids[count]
@@ -143,6 +151,10 @@ def data():
                 shopping_list_dataframe.to_sql("shopping_list", con=engine, if_exists="replace")
             count = count + 1
 
+        for specific_remove_id in personal_remove_ids:
+            if(request.form.get(specific_remove_id)):
+                return(specific_remove_id)
+
     aldistring = " "
     colesstring = " "
     count = 0
@@ -153,24 +165,17 @@ def data():
             aldistring = aldistring + '<input type="submit" value="'+item+'" name="'+weird_ids[count]+'"/></br></br>'
         if(shops[item] == "Coles"):
             colesstring = colesstring + '<input type="submit" value="'+item+'" name="'+weird_ids[count]+'"/></br></br>'
-        count = count + 1
-
-    first_layer.append(aldistring)
-    first_layer.append(colesstring)
+        count = count + 1 
 
     option_string = " "
-
     for item in all_items:
         option_string = option_string + '<option value="'+item+'">'
 
+    first_layer = []
+    first_layer.append(aldistring)
+    first_layer.append(colesstring)
     first_layer.append(option_string)
     first_layer.append(option_string)
-
-    recipe_string = " "
-
-    for instruction in recipes_dataframe["instructions"]:
-        recipe_string = recipe_string + '<div id="recipe">' + instruction + "</br></br><input type='submit' value='remove'/></br></br><input type='submit' value='edit'/></br></br></div></br>"
-
     first_layer.append(recipe_string)
 
     return render_template_string(stringinserter("@",page1,first_layer), kart_form=kart_form, add_form=add_form, remove_form=remove_form)
